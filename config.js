70ffdf6680def9aaa4c82d3f89e3a83752a57b7c
samupsigamer <samu.psigamer@gmail.com>
2026-08-29 21:37:01 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Configuración sencilla del Modelo 4 · Versión B
 // Cambia aquí la imagen inicial de cada sección.
-// Valores disponibles: laptop, headset, tablet, reading
+// Valores disponibles: laptop, headset, tablet, reading, prueba
 window.SAMU_CONFIG = {
   sectionImages: {
     sobreMi: "laptop",
